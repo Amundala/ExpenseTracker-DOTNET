40ac16f06029485cc19cdffaea411757f5d26b4b
Tom Amundala <100129520+toma2k@users.noreply.github.com>
2023-09-30 10:20:15 +0300

    First oush

---
+++ Expenses Tracker/Models/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Expenses_Tracker.Models
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions options):base(options)
+        {
+            
+        }
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ Expenses Tracker/Models/Transaction.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Expenses_Tracker.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int TransactionId { get; set; }
+        //catIdFK
+        [Range(1, int.MaxValue, ErrorMessage  = "Please Must choose a category")]
+        public int CategoryId { get; set; }
+        public Category? Category { get; set; }
+        [Range(1, int.MaxValue, ErrorMessage = "Shyiramo positive amount mwana!")]
+        public int Amount { get; set; }
+        [Column(TypeName = "nvarchar(50)")]
+        public string? Note { get; set; }
+        public DateTime Date { get; set; } = DateTime.Now;
+        [NotMapped]
+        public string? CategoryWithIcon { 
+            get {
+               return Category == null ? "" : Category.Icon + " " + Category.Title;
+            }
+        }
+        [NotMapped]
+        public string? FormattedAmount { 
+            get {
+                return ((Category == null || Category.Type == "Expense") ? " - " : " + ") + Amount.ToString("C0");
+            } 
+        }
+    }
+}
